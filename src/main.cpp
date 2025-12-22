a28815570518b8a3f509e29cc4cd4ae5a183973b
AQchance <1363616727@qq.com>
2025-12-22 11:05:20 +0800

    add logs for at the start and the end of the main function

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,9 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+  LOG_INFO("Mini LSM started with %d arguments", argc);
+  
   std::cout << "Hello, Mini LSM!\n";
+
+  LOG_INFO("Mini LSM exited successfully");
 }
