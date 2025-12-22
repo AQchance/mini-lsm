f49920293c919eae8e4d2cedb7884fce8f35dd4c
AQchance <1363616727@qq.com>
2025-12-22 11:19:33 +0800

    change log format while outputing test result

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,15 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  LOG_INFO("Mini LSM started with %d arguments", argc);
-  
+  // init logger
+  mini_lsm::Logger &logger = mini_lsm::Logger::get_instance();
+  logger.set_log_file("mini_lsm.log", mini_lsm::Logger::DEFAULT_MAX_FILE_SIZE,
+                      mini_lsm::Logger::DEFAULT_MAX_FILE_COUNT);
+  logger.set_log_level(mini_lsm::LogLevel::DEBUG);
+
+  LOG_INFO("\nMini LSM started with %d arguments\n", argc);
+
   std::cout << "Hello, Mini LSM!\n";
 
-  LOG_INFO("Mini LSM exited successfully");
+  LOG_INFO("\nMini LSM exited successfully\n");
 }
